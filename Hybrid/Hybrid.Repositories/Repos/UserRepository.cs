715fdf294438cb15dba3fa8d2ef40ed5ba282a04
TriNHM <triminh0502@gmail.com>
2025-05-14 22:44:30 +0700

    [ADD] add Login api and RefreshToken api
    Api RefreshToken using to regenerate access token

---
+++ Hybrid/Hybrid.Repositories/Repos/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using Hybrid.Repositories.Base;
+using Hybrid.Repositories.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hybrid.Repositories.Repos
+{
+    public class UserRepository : GenericRepository<User>
+    {
+        public UserRepository(HybridDBContext context) : base(context)
+        {
+
+        }
+
+        public async Task<User?> GetUserAsync(string email, string password)
+        {
+            var user = await _dbSet.Include(x => x.Role)
+                                   .FirstOrDefaultAsync(x => x.Email == email &&
+                                                             x.Password == password &&
+                                                             x.IsActive);
+            return user;
+        }
+
+        public async Task<User?> GetByRefreshTokenAsync(string refreshToken)
+        {
+            var user = await _dbSet.FirstOrDefaultAsync(x => x.RefreshToken == refreshToken &&
+                                                             x.IsActive);
+            return user;
+        }
+    }
+}
---
+++ Hybrid/Hybrid.Services/ViewModel/LoginResponse.cs
@@ -0,0 +1,20 @@
+﻿using Hybrid.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hybrid.Services.ViewModel
+{
+    public class LoginResponse
+    {
+        public string? UserId { get; set; }
+        public string? RoleId { get; set; }
+        public string? RoleName { get; set; }
+        public string? AccessToken { get; set; }
+        public string? RefreshToken { get; set; }
+
+
+    }
+}
