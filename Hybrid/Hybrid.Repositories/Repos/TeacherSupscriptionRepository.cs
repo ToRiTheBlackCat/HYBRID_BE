2133fdb2dc75056c1d9ae24eaa2e8376fd4bd3b4
TriNHM <triminh0502@gmail.com>
2025-05-21 22:05:16 +0700

    [ADD] add CreateSupscriptionExtention for both site
    Not have payment api yet

---
+++ Hybrid/Hybrid.Repositories/Repos/TeacherSupscriptionRepository.cs
@@ -0,0 +1,18 @@
+﻿using Hybrid.Repositories.Base;
+using Hybrid.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hybrid.Repositories.Repos
+{
+    public class TeacherSupscriptionRepository : GenericRepository<TeacherSupscription>
+    {
+        public TeacherSupscriptionRepository(HybridDBContext context) : base(context)
+        {
+
+        }
+    }
+}
