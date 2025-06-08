9d2ca21cbbbb87e506c410de305e74a49f5198fb
TriNHM <triminh0502@gmail.com>
2025-06-08 17:15:56 +0700

    [ADD] add GetAllRatingOfMinigame and CreateRating api

---
+++ Hybrid/Hybrid.Repositories/Repos/RatingRepository.cs
@@ -0,0 +1,37 @@
+﻿using Hybrid.Repositories.Base;
+using Hybrid.Repositories.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hybrid.Repositories.Repos
+{
+    public class RatingRepository : GenericRepository<Rating>
+    {
+        public RatingRepository(HybridDBContext context) : base(context)
+        {
+        }
+
+        public async Task<List<Rating>?> GetAllByMinigameId(string minigameId)
+        {
+            var ratingList = await _context.Ratings
+                .Where(x => x.MinigameId.Equals(minigameId))
+                .Include(x => x.Student)
+                .ToListAsync();
+
+            return ratingList;
+        }
+        
+        public async Task<Rating?> GetExistedRating(string minigameId, string studentId)
+        {
+            var foundRating = await _context.Ratings
+                .SingleOrDefaultAsync(x => x.MinigameId.Equals(minigameId) && x.StudentId.Equals(studentId));   
+
+            return foundRating;
+        }
+
+    }
+}
---
+++ Hybrid/Hybrid.WebAPI/Controllers/RatingController.cs
@@ -0,0 +1,64 @@
+﻿using Hybrid.Repositories.Models;
+using Hybrid.Services.Services;
+using Hybrid.Services.ViewModel.Rating;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hybrid.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RatingController : ControllerBase
+    {
+        private readonly IRatingService _ratingService;
+        public RatingController(IRatingService ratingService)
+        {
+            _ratingService = ratingService;
+        }
+
+        /// <summary>
+        /// API_GetAllRatingOfMinigame
+        /// minigameId - string?
+        /// List<Rating>
+        /// Created By: TriNHM
+        /// Created Date: 8/6/2025
+        /// Updated By: X
+        /// Updated Date: X
+        /// </summary>
+        [HttpGet("{minigameId}")]
+        public async Task<ActionResult<List<GetAllRatingResponse>>> GetAllRatingOfMinigame(string minigameId)
+        {
+            var ratingList = await _ratingService.GetAllRatingByMinigameId(minigameId);
+
+            return Ok(ratingList);
+        }
+
+        /// <summary>
+        /// API_RatingMinigame
+        /// CreateRatingRequest_ViewModel
+        /// Rating
+        /// Created By: TriNHM
+        /// Created Date: 8/6/2025
+        /// Updated By: X
+        /// Updated Date: X
+        /// </summary>
+        [HttpPost]
+        public async Task<ActionResult<Rating>> RatingMinigame(CreateRatingRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var (isSuccess, message) = await _ratingService.CreateRating(request);
+
+
+            return Ok(new
+            {
+                isSuccess,
+                message
+            });
+
+        }
+    }
+}
