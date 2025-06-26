626047f823136bb3dabca2f5dd5ab0dd9397c3c8
TriNHM <triminh0502@gmail.com>
2025-06-26 23:56:15 +0700

    [ADD] add model FilterUsersResponse

---
+++ Hybrid/Hybrid.Services/ViewModel/User/FilterUsersResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hybrid.Services.ViewModel.User
+{
+    public class FilterUsersResponse
+    {
+        public string UserId { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public string RoleId { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
